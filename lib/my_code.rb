ea1ab94374f36870ca4bd0bb4086e110cdd8ca14
Steven Frasica <sfrasica1@gmail.com>
2019-12-31 04:30:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,13 @@ def map_to_no_change(source_array)
   end
   return new
 end
+
+def map_to_double
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(source_array[i] *2)
+    i +=1
+  end
+   return new
+ end
