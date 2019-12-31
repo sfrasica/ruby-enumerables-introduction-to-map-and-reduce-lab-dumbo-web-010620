fa779f51f3b4ec63a5314652755b526c0af3c923
Steven Frasica <sfrasica1@gmail.com>
2019-12-31 04:28:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ def map_to_negativize(source_array)
   end
   return new
 end
+
+def map_to_no_change(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] )
+    i += 1
+  end
+  return new
+end
