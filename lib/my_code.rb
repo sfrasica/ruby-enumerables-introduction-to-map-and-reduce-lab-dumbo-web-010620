b741a01e00e5e78ba0f24ce6e61dde60247895e9
Steven Frasica <sfrasica1@gmail.com>
2019-12-31 04:33:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,3 +28,13 @@ def map_to_double(source_array)
   end
    return new
  end
+
+ def map_to_square(source_array)
+   new = []
+   i = 0
+   while i < source)_array.length do
+     new.push( source_array[i] **2)
+     i += 1
+   end
+    return new
+  end
