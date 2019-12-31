f73727fd151e29f9a559cce994a50f70860e0ec9
Steven Frasica <sfrasica1@gmail.com>
2019-12-31 04:35:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,3 +38,13 @@ def map_to_double(source_array)
    end
     return new
   end
+
+  def reduce_to_total(source_array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
